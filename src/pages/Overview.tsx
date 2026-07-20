c24707cc5e402e8289e2fdf27102f3c1d4310b13
kurokaw3a <kamaldinov321@gmail.com>
2026-07-21 03:45:42 +0600

    master

--- src/pages/Overview.tsx
+++ src/pages/Overview.tsx
@@ -15,7 +15,7 @@ export const OverviewPage = ({ tv }: { tv?: boolean }) => {
           style={{
             backgroundImage: `url('https://image.tmdb.org/t/p/w1920_and_h1080_multi_faces/${data.backdrop_path}')`,
           }}
-          className={`absolute z-0 left-0 top-0 w-full h-screen bg-cover bg-center shadow-[inset_0px_90px_90px_30px_#1a1a1a] blur-xs`}
+          className={`absolute -z-1 left-0 top-0 w-full h-screen bg-cover bg-center shadow-[inset_0px_90px_90px_30px_#1a1a1a] blur-xs`}
         ></div>
         <img
           className="w-40 md:w-80"
